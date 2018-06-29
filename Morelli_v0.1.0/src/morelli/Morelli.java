75e642c6aa59a59c65a8ba2fbf772535adf612a5
Adriano Lima <adrianolslima@gmail.com>
2018-06-28 22:42:21 -0300

    Versão final

---
+++ Morelli_v0.1.0/src/morelli/Morelli.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package morelli;
+
+import interfaceGrafica.AtorJogador;
+
+/**
+ *
+ * @author Adriano
+ */
+public class Morelli {
+
+	public static void main(String args[]) {
+
+		AtorJogador jogador = new AtorJogador();
+
+	}
+
+}
