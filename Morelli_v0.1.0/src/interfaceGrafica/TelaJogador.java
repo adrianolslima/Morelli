d140a3269b576b2cf0bc7d8d4e62e69620752694
Adriano Lima <adrianolslima@gmail.com>
2018-09-05 19:41:23 -0300

    Versao final

--- Morelli_v0.1.0/src/interfaceGrafica/TelaJogador.java
+++ Morelli_v0.1.0/src/interfaceGrafica/TelaJogador.java
@@ -29,7 +29,6 @@ public class TelaJogador extends javax.swing.JFrame {
         initComponents();
         this.defineFaixas();
 
-//        this.inserePreta(botoesVermelhos, 1);
         jogo = atorJogador;
     }
 
